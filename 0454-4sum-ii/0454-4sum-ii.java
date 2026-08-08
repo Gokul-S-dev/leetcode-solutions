09b28f17090d75071bfbd7273feceb3a7500981d
Gokul S <sggokul762@gmail.com>
2026-08-08 11:34:38 +0530

    Time: 111 ms (58.39%), Space: 47.3 MB (5.76%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int a:nums1){
+            for(int b:nums2){
+                int sum = a+b;
+                mp.put(sum,mp.getOrDefault(sum,0)+1);
+            }
+        }
+        int count=0;
+        for(int c:nums3){
+            for(int d:nums4){
+                int sum = c+d;
+                int complement = -sum;
+                count+=mp.getOrDefault(complement,0);
+            }
+        }
+
+        return count;
+        
+    }
+}
