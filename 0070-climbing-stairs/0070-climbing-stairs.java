ef6567bbc4f14d84c121458257445ecf67ed6ab7
Gokul S <sggokul762@gmail.com>
2026-08-31 09:42:50 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.75%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int check(int n, int[] dp){
+        if(n==1 || n==2){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n]=check(n-1,dp)+check(n-2,dp);
+        return dp[n];
+    }
+    
+    public int climbStairs(int n) {
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+       return  check(n,dp);
+        
+    }
+}
