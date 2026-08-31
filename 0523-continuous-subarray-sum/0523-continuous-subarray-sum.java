77425e82ae29d0c7d4ed2d006dc35d8782ebac3c
Gokul S <sggokul762@gmail.com>
2026-08-31 20:25:32 +0530

    Time: 24 ms (62.36%), Space: 140 MB (18.93%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Remainder 0 occurs before the array starts
+        map.put(0, -1);
+
+        int prefixSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            prefixSum += nums[i];
+
+            int remainder = prefixSum % k;
+
+            if (map.containsKey(remainder)) {
+
+                // Check subarray length >= 2
+                if (i - map.get(remainder) >= 2) {
+                    return true;
+                }
+
+            } else {
+                // Store only the first occurrence
+                map.put(remainder, i);
+            }
+        }
+
+        return false;
+    }
+}
